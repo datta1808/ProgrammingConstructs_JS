459767ccad70a73b22350107d97f13e120ad0595
datta1808 <bagaledatta18@gmail.com>
2021-06-30 13:50:40 +0530

    ADDED : Program to Print nth Harmonic Number

---
+++ for_loop Statement/harmonicNumber.js
@@ -0,0 +1,13 @@
+// To  take a command-line argument n and print the nth harmonic number
+
+    const prompt = require('prompt-sync')();
+
+    let number = Number(prompt("Enter a number : "));
+
+    let harmonicNumber = 1;
+
+    for (let index = 2; index <= number; index++) {
+        harmonicNumber += 1 / index;
+    }
+
+    console.log("Harmonic Number is " + harmonicNumber);
