2c7b799946bb5f0faad8e4e6806ee7666757265a
datta1808 <bagaledatta18@gmail.com>
2021-06-30 13:39:37 +0530

    ADDED : Program to Print a table of powers of 2

---
+++ for_loop Statement/program1.js
@@ -0,0 +1,9 @@
+// To take a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n
+
+const prompt = require('prompt-sync')();
+
+let number = Number(prompt("Enter a number : "));
+
+for (let index = 0; index <= number; index++) {
+    console.log(`${Math.pow(2, index)}`);
+}
