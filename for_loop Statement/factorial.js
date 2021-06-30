a1b5a4b6c03d2f42b1e936de64a1a610b1ad97e6
datta1808 <bagaledatta18@gmail.com>
2021-06-30 15:22:32 +0530

    ADDED : Program to find the Factorial of a Number

---
+++ for_loop Statement/factorial.js
@@ -0,0 +1,13 @@
+// To compute a factorial of a number taken as input
+
+    const prompt = require('prompt-sync')();
+
+    let number = Number(prompt("Enter a number : "));
+
+    let factorial = 1;
+
+    for (let index = 1; index <= number; index++) {
+        factorial = factorial * index;
+    }
+
+    console.log(`Factorial of ${number} is ${factorial}`);
